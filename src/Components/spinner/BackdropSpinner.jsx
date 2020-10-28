25623c1897612e730182972a056576055d4b0d8c
Sebastian Vasco <sserna75@misena.edu.co>
2020-10-28 10:38:12 -0500

    Changes in RegularSpinner.jsx and BackdropSpinner.jsx to recieve children as props

--- src/Components/spinner/BackdropSpinner.jsx
+++ src/Components/spinner/BackdropSpinner.jsx
@@ -3,15 +3,17 @@ import Loader from "react-loader-spinner";
 import { useTheme } from "@material-ui/core/styles";
 import { useSpinnerStyles } from "Assets/Styles/spinnerStyles";
 
-const BackdropSpinner = () => {
+const BackdropSpinner = ({ isLoading, children }) => {
   const theme = useTheme();
   const classes = useSpinnerStyles();
 
-  return (
+  const Spinner = (
     <Backdrop open className={classes.backdrop}>
       <Loader type="Bars" color={theme.palette.secondary.main} height={100} width={100} />
     </Backdrop>
   );
+
+  return <>{isLoading ? Spinner : children}</>;
 };
 
 export default BackdropSpinner;
--- src/Components/spinner/RegularSpinner.jsx
+++ src/Components/spinner/RegularSpinner.jsx
@@ -1,14 +1,16 @@
 import { CircularProgress } from "@material-ui/core";
 import { useSpinnerStyles } from "Assets/Styles/spinnerStyles";
 
-const RegularSpinner = ({ size = 50 }) => {
+const RegularSpinner = ({ size = 50, isLoading, children }) => {
   const classes = useSpinnerStyles();
 
-  return (
+  const Spinner = (
     <div className={classes.root}>
       <CircularProgress color="secondary" size={size} />
     </div>
   );
+
+  return <>{isLoading ? Spinner : children}</>;
 };
 
 export default RegularSpinner;
