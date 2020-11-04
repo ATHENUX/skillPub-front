9bbb2483037d592c38c8697608ad107a8b99ee2b
blopa2000 <jpagudelo2000@gmail.com>
2020-11-03 23:35:36 -0500

    Axios configuration

--- src/Components/access/SignIn.jsx
+++ src/Components/access/SignIn.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Typography, TextField, Button, Link, Hidden } from "@material-ui/core";
 import { useAccessStyleTheme } from "Assets/Styles/accessStyles";
 
@@ -7,13 +8,34 @@ import Social from "./Social";
 //i18n
 import { useTranslation } from "react-i18next";
 
+//axios
+import axios from "axiosConfig";
+
 const SignIn = ({ changeAccess }) => {
-  const { t } = useTranslation();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const classes = useAccessStyleTheme();
+  const { t } = useTranslation();
+
+  const handleSignIn = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await axios.post("/signIn", { email, password });
+      const { success } = res.data;
+      if (success) {
+        // localStorage.setItem("session");
+        console.log(res.data);
+      } else {
+        console.log(res.data);
+      }
+    } catch (error) {
+      console.log("eerorr", error);
+    }
+  };
 
   return (
     <div className={classes.signInContainer}>
-      <form className={`${classes.formStyle} ${!changeAccess ? classes.hideSingIn : ""}`}>
+      <form onSubmit={handleSignIn} className={`${classes.formStyle} ${!changeAccess ? classes.hideSingIn : ""}`}>
         <Hidden xsDown>
           <Typography variant="h3">{t("sign.in")}</Typography>
         </Hidden>
@@ -22,12 +44,12 @@ const SignIn = ({ changeAccess }) => {
         </Hidden>
         <Social />
         <Typography variant="caption">{t("use.accounts")}</Typography>
-        <TextField type="email" id="Email" label={t("email")} variant="outlined" size="small" />
-        <TextField type="password" id="password" label={t("password")} variant="outlined" size="small" />
+        <TextField type="email" id="Email" label={t("email")} variant="outlined" size="small" value={email} onChange={(e) => setEmail(e.target.value)} />
+        <TextField type="password" id="password" label={t("password")} variant="outlined" size="small" value={password} onChange={(e) => setPassword(e.target.value)} />
         <div className={classes.link}>
           <Link href="#">{t("recover.account")}</Link>
         </div>
-        <Button variant="contained" color="primary">
+        <Button type="submit" variant="contained" color="primary">
           {t("sign.in")}
         </Button>
       </form>
