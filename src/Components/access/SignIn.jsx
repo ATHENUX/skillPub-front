b4a26e8229e9c1385e01a2856e5aa31a32541312
dazuluaga <zuluagaosorio987@gmail.com>
2020-11-10 12:39:53 -0500

    Show password

--- src/Components/access/SignIn.jsx
+++ src/Components/access/SignIn.jsx
@@ -2,10 +2,23 @@ import { useState } from "react";
 import { useHistory } from "react-router-dom";
 
 //material ui
-import { Typography, TextField, Button, Hidden, Snackbar } from "@material-ui/core";
+import {
+  Typography,
+  TextField,
+  Button,
+  Hidden,
+  Snackbar,
+  OutlinedInput,
+  InputAdornment,
+  IconButton,
+  InputLabel,
+  FormControl,
+} from "@material-ui/core";
 import MuiAlert from "@material-ui/lab/Alert";
 import { useAccessStyle } from "Assets/Styles/accessStyles";
 import useCustomStyles from "Assets/Styles/CustomStyles";
+import Visibility from "@material-ui/icons/Visibility";
+import VisibilityOff from "@material-ui/icons/VisibilityOff";
 
 //react-form
 import { useForm } from "react-hook-form";
@@ -26,6 +39,7 @@ const SignIn = ({ changeAccess }) => {
     show: false,
     message: "",
   };
+  const [values, setValues] = useState(false);
   const [snackBar, setSnackBar] = useState(initialSnackBarProps);
   const [isLoading, setIsLoading] = useState(false);
   const { register, handleSubmit, errors } = useForm();
@@ -33,6 +47,8 @@ const SignIn = ({ changeAccess }) => {
   const customStyles = useCustomStyles();
   const history = useHistory();
   const { t } = useTranslation();
+
+  console.log(errors?.password);
 
   const handleSignIn = async (data) => {
     const { email, password } = data;
@@ -67,6 +83,18 @@ const SignIn = ({ changeAccess }) => {
     setSnackBar(initialSnackBarProps);
   };
 
+  const handleChange = (prop) => (event) => {
+    setValues(values);
+  };
+
+  const handleClickShowPassword = () => {
+    setValues(!values);
+  };
+
+  const handleMouseDownPassword = (event) => {
+    event.preventDefault();
+  };
+
   return (
     <>
       <div className={classes.signInContainer}>
@@ -95,18 +123,36 @@ const SignIn = ({ changeAccess }) => {
             })}
           />
           <div className={customStyles.messageInput}>{errors?.email?.message}</div>
-          <TextField
-            type="password"
-            name="password"
-            label={t("password")}
-            variant="outlined"
-            size="small"
-            error={Boolean(errors?.password)}
-            inputRef={register({
-              required: { value: true, message: t("password.message.error.input.required") },
-              minLength: { value: 6, message: t("password.message.error.input.short") },
-            })}
-          />
+          <FormControl className={classes.formStylePassword} size="small" variant="outlined">
+            <InputLabel error={Boolean(errors?.password)} htmlFor="outlined-adornment-password">
+              {t("password")}
+            </InputLabel>
+            <OutlinedInput
+              className={classes.passwordInput}
+              id="outlined-adornment-password"
+              type={values ? "text" : "password"}
+              name="password"
+              onChange={handleChange("password")}
+              labelWidth={70}
+              error={Boolean(errors?.password)}
+              endAdornment={
+                <InputAdornment position="end" size="small">
+                  <IconButton
+                    aria-label="toggle password visibility"
+                    onClick={handleClickShowPassword}
+                    onMouseDown={handleMouseDownPassword}
+                    edge="end"
+                  >
+                    {values ? <Visibility /> : <VisibilityOff />}
+                  </IconButton>
+                </InputAdornment>
+              }
+              inputRef={register({
+                required: { value: true, message: t("password.message.error.input.required") },
+                minLength: { value: 6, message: t("password.message.error.input.short") },
+              })}
+            />
+          </FormControl>
           <div className={customStyles.messageInput}>{errors?.password?.message}</div>
 
           <RecoverPassword />
