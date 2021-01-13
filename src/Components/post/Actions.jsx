39b6ba491d5f7d6fd49d2a0b13a6815cc4735dc5
Sebastian Vasco <skaterionegro9@gmail.com>
2021-01-13 18:33:46 -0500

    Home posts components and functionality

--- src/Components/post/Actions.jsx
+++ src/Components/post/Actions.jsx
@@ -1,32 +1,38 @@
 //material-UI
-import { CardActions, IconButton, Typography } from "@material-ui/core";
+import { CardActions, IconButton } from "@material-ui/core";
 import { usePostStyles } from "Assets/Styles/postsStyles";
 import FavoriteBorderIcon from "@material-ui/icons/FavoriteBorder";
-import TextsmsOutlinedIcon from "@material-ui/icons/TextsmsOutlined";
+//import TextsmsOutlinedIcon from "@material-ui/icons/TextsmsOutlined";
 import OpenInNewRoundedIcon from "@material-ui/icons/OpenInNewRounded";
-import BookmarkBorderRoundedIcon from "@material-ui/icons/BookmarkBorderRounded";
+//import BookmarkBorderRoundedIcon from "@material-ui/icons/BookmarkBorderRounded";
+import FavoriteIcon from "@material-ui/icons/Favorite";
 
-const Actions = ({ likes, comments, republishValue, favorites, handleRepublish }) => {
+const Actions = ({ liked, handleRepublish, handleLike, handleDislike }) => {
   const classes = usePostStyles();
 
   return (
     <CardActions className={classes.CardAction}>
-      <IconButton aria-label="likes">
-        <FavoriteBorderIcon />
-        <Typography>{likes}</Typography>
-      </IconButton>
-      <IconButton aria-label="comments">
+      {!liked ? (
+        <IconButton aria-label="likes" onClick={handleLike}>
+          <FavoriteBorderIcon />
+        </IconButton>
+      ) : (
+        <IconButton aria-label="likes" onClick={handleDislike}>
+          <FavoriteIcon />
+        </IconButton>
+      )}
+
+      {/* <IconButton aria-label="comments">
         <TextsmsOutlinedIcon />
         <Typography>{comments}</Typography>
-      </IconButton>
+      </IconButton> */}
       <IconButton aria-label="share" onClick={handleRepublish}>
         <OpenInNewRoundedIcon />
-        <Typography>{republishValue}</Typography>
       </IconButton>
-      <IconButton aria-label="favorite">
+      {/* <IconButton aria-label="favorite">
         <BookmarkBorderRoundedIcon />
         <Typography>{favorites}</Typography>
-      </IconButton>
+      </IconButton> */}
     </CardActions>
   );
 };
